8ae8432d93a74d38943fc47f182b624d86011b30
yongonyon <ryanharpergonyon@gmail.com>
2019-04-10 12:58:10 -0400

    1 file modified

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
-
 import Game from './TicTacToe/Game'
-import './style.css';
 
 render(<Game />, document.getElementById('root'));
