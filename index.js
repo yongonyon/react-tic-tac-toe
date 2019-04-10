eb73bdf646cd9b731db5a3bb9fce41118d8f54b2
yongonyon <ryanharpergonyon@gmail.com>
2019-04-10 12:49:00 -0400

    2 files modified, 1 deleted

--- index.js
+++ index.js
@@ -1,24 +1,7 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
-import Hello from './Hello';
-import ShoppingList from './ShoppingList';
 
 import Game from './TicTacToe/Game'
 import './style.css';
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      name: 'React'
-    };
-  }
-
-  render() {
-    return (
-      <Game/>
-    );
-  }
-}
-
-render(<App />, document.getElementById('root'));
+render(<Game />, document.getElementById('root'));
